385188fd896f068d02fdc02cc578b7e9feadb9ee
boggdan95 <bandabog@gmail.com>
2018-07-04 19:50:38 -0600

    models created
    Training Type,  Training Description, Training Session, PresetTraininigSession

--- training/models.py
+++ training/models.py
@@ -2,32 +2,43 @@ from django.db import models
 
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+from django.contrib.postgres.fields import JSONField
+
 
 # Create your models here.
 
 class TrainingType(models.Model):
-    id = models.IntegerField()
-    key =
-    name =
+    id = models.AutoField(primary_key=True)
+    key = models.PositiveIntegerField()
+    name = (('s' , "Secuencial"),('r',"Aleatorio"))
 
 
 
 class TrainingDescription(models.Model):
-    id = models.IntegerField(default=0)
+    id = models.AutoField(primary_key=True)
     type = models.ForeignKey(TrainingType, on_delete=models.CASCADE)
-    user =
-    name = models.
-    parameters =
+    user = User.get_username
+    name = models.TextField()
+    parameters = ('Time', models.IntegerField()),('Iteration', models.IntegerField()),('Hit', models.BooleanField(), type)
 
 
 
 class TrainingSession(models.Model):
-    id =
-    key =
+    id = models.AutoField(primary_key=True)
+    time = models.DateTimeField(auto_now_add=True)
+    user = User.get_username
+    description = models.ForeignKey(TrainingDescription, on_delete=models.CASCADE)
+    results = JSONField() 
 
 
 
 class PresetTrainingSession(models.Model):
-    id =
-    key =
-    name =
+    id = models.AutoField(primary_key=True)
+    key = models.IntegerField()
+    type = models.ForeignKey(TrainingType, on_delete=models.CASCADE)
+    parameters = ('Time', models.IntegerField()),('Iteration', models.IntegerField()),('Hit', models.BooleanField(), type)
+
+
+
+
