db748e9b54ac65fb097b49bf2a9c9d0dfab803d3
boggdan95 <bandabog@gmail.com>
2018-07-05 19:47:09 -0600

    sql migration, view & url begin

--- training/admin.py
+++ training/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import TrainingType, TrainingDescription, TrainingSession, PresetTrainingSession
+
+admin.site.register(TrainingType)
+admin.site.register(TrainingDescription)
+admin.site.register(TrainingSession)
+admin.site.register(PresetTrainingSession)
--- training/views.py
+++ training/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
 
-from django.urls import path
+from django.contrib.auth.models import User
 
-from . import views
 
-urlpatterns = [
-    path('', views.index, name='index'),
-]
+
+def HomeView(request):
+    user = User.get_username
+    return render(request, 'trainig/homepage.html', {'user' : user })
+
