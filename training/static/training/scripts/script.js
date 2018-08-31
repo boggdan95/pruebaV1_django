16854bb8fba950b5cbdbe8eb0e2afc18d838bf73
boggdan95 <bandabog@gmail.com>
2018-08-31 13:57:17 -0600

    conection to sockets

--- training/static/training/scripts/script.js
+++ training/static/training/scripts/script.js
@@ -1,40 +1,37 @@
-var nombre, apellido, edad, tipoEntrenamiento, tiempoEntrenamiento, modulosEntrenamiento, moduloColor;
+var socket = io('http://localhost:5000');
 
-function athlete(nombre,apellido,edad){
-  this.nombre = nombre;
-  this.apellido  = apellido;
-  this.edad = edad;
+var description_decode;
+
+function startTraining() {
+  document.getElementById("button_start").style.display = "none";
+  var description = document.getElementById("hide_description").textContent;
+  description_decode = JSON.parse(description);
+  tiempo = description_decode.time;
+  secuencial = description_decode.is_secuencial;
+  reps = description_decode.reps;
+  modules = description_decode.modules;
+  tipo = description_decode.typeReaction;
+  console.log(tiempo, secuencial, reps, modules, tipo);
+  temporizador(tiempo);
+  socket.emit('hi','hola');
+  //var entrenamiento = { "Reaccion":tipoEntrenamiento, "Tiempo":tiempoEntrenamiento, "Modulo":modulosEntrenamiento};
+  //var myJSON = JSON.stringify(entrenamiento);
+  //socket.emit("designTrainnig",myJSON);
 }
 
 
-function randomInt (low, high) {
-    return Math.floor(Math.random() * (high - low + 1) + low);
+
+//Insertar resultados
+function introduceResults() {
+  var table = document.getElementById("results_table");
+  var row = table.insertRow(-1);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  var cell3 = row.insertCell(2);
+  cell1.innerHTML = "NEW CELL1";
+  cell2.innerHTML = "NEW CELL2";
+  cell3.innerHTML = "NEW CELL3";
 }
-
-function startTrainning() {
-  typeOfTrainning();
-  var entrenamiento = { "Reaccion":tipoEntrenamiento, "Tiempo":tiempoEntrenamiento, "Modulo":modulosEntrenamiento};
-  var myJSON = JSON.stringify(entrenamiento);
-  socket.emit("designTrainnig",myJSON);
-}
-
-
-//Entrenamiento seleccionado
-function typeOfTrainning(){
-  var tipo = document.getElementById("reaccion");
-  var tiempo = document.getElementById('tiempo');
-  var modulos = document.getElementById('modulo');
-
-  tipoEntrenamiento = tipo[tipo.selectedIndex].text;
-  tiempoEntrenamiento = tiempo[tiempo.selectedIndex].text;
-  modulosEntrenamiento = modulos[modulos.selectedIndex].text;
-
-  console.log(tipoEntrenamiento);
-  console.log(tiempoEntrenamiento);
-  console.log(modulosEntrenamiento);
-
-}
-
 
 
 
@@ -58,4 +55,33 @@ function colorSelect() {
     moduloColor = "green";
   }
   console.log(moduloColor);
+}
+
+
+function temporizador(time){
+  var tiempo = time;
+  var x = setInterval(function(){
+
+    minutes = (tiempo / 60) | 0;
+    seconds = (tiempo % 60) | 0;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    document.getElementById("timer").innerHTML = minutes +" : " + seconds;
+    tiempo--;
+
+    if(seconds == 00 && minutes > 0){
+      minutes--;
+      seconds = 60;
+      if (minutes == 0){
+         minutes = 0;
+      }
+    }
+    if (seconds <= 00 && minutes <= 0) {
+      clearInterval(x);
+      document.getElementById("timer").innerHTML = "Entrenamiento finalizado";
+      document.getElementById("btn_home").style.display = "block";
+      document.getElementById("btn_results").style.display = "block";
+    }
+
+  },1000);
 }
